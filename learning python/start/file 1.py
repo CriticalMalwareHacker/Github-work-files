c507a7f5ceca03c5d0f025ca106784c8afab4ab8
Tanay Kumar <tanay25000@gmail.com>
2024-05-15 13:17:26 +0530

    new changes

---
+++ learning python/start/file 1.py
@@ -0,0 +1,7 @@
+print('hello world')
+print('I am Tanay Kumar')
+print('piacb '*10)
+a=20
+floaty=5.9
+is_published=True 
+print(a)
---
+++ learningpython/start/variables.py
@@ -0,0 +1,12 @@
+course = "Python 101"
+name = "Tanay"
+print(course)
+print(course)
+print('My name is',name)
+
+if course == "Python 101":
+    pass
+
+#This is a comment  
+#indentation is important don't push your code to the right keep it to the left
+#TODO:This is used to manage tasks
