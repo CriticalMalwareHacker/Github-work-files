e3c9f5aecee8ccf95a01ee999d81b67e3a9aadfb
Tanay Kumar <72481758+CriticalMalwareHacker@users.noreply.github.com>
2024-05-16 15:08:33 +0530

    Add files via upload

---
+++ learning python/start/integers.py
@@ -0,0 +1,10 @@
+items = 4
+price = 20
+string = "tanay"
+total = items*price
+total2 = items*string #multiplies the "tanay" string 4 times
+print(total)
+print(total2)
+#can also multiply within the print statement
+print(total2*5)
+print(1_000_000_000) #python doesnt care if you add underscore within the integer, it will simply ignore it and just print the number
