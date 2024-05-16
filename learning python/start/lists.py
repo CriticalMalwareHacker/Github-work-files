05e8dfae1048df5d4d0064a7ec4b4891ea2e2e07
Tanay Kumar <72481758+CriticalMalwareHacker@users.noreply.github.com>
2024-05-16 15:37:40 +0530

    Add files via upload

---
+++ learning python/start/lists.py
@@ -0,0 +1,5 @@
+lst=["STring",1,3.5,["A new item"],"Tanay"]#lists store multiple values including variables
+print(lst)#it printed exactly what we wrote
+
+for item in lst:#for loop
+    print("The item is =",item)
