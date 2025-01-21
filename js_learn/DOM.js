612b63fd8dc6518b44fe0b7244c706a22f641ff4
Tanay Kumar <tanay25000@gmail.com>
2025-01-21 23:27:39 +0530

    learning_cssgrid

--- js_learn/DOM.js
+++ js_learn/DOM.js
@@ -1,5 +1,11 @@
 //DOM is Document Object Model
 <div id="container">
-  <div class="display"></div>
-  <div class="controls"></div>
-</div>
+  <div class="display">hey</div>
+  <div class="controls">yo</div>
+</div>                                        
+// selects the .controls div
+const controls = document.querySelector(".controls");
+
+// selects the prior sibling => .display
+const display = controls.previousElementSibling;
+console.log(display); // <div class="display"></div>    
